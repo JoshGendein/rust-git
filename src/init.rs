dfb9a68d592779403f1d3e2c74635b6b888e29f2
Josh Gendein <joshua@gendein.com>
2020-06-12 12:01:36 -0700

    initialize .gitr dir

---
+++ src/init.rs
@@ -0,0 +1,19 @@
+use super::error::Error;
+
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+pub fn init() -> Result<(), Error> {
+    let dir = Path::new(".gitr");
+
+    fs::create_dir(dir)?;
+    fs::create_dir(dir.join("objects"))?;
+    fs::create_dir(dir.join("refs"))?;
+    fs::create_dir(dir.join("refs").join("heads"))?;
+
+    let mut head = File::create(dir.join("HEAD"))?;
+    head.write_all("refs: refs/heads/master".as_bytes())?;
+    Ok(())
+}
