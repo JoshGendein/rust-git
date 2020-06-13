42e8b7c2252612e8de8789ea5291c53b9fa2a6e2
Josh Gendein <joshua@gendein.com>
2020-06-13 15:53:34 -0700

    added clap support for init and add.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod error;
+mod init;
+mod index;
+mod types;
+mod file;
+mod add;
+
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let m = App::new("gitr")
+        .subcommand(SubCommand::with_name("init").about("Initialize the repo"))
+        .subcommand(SubCommand::with_name("add")
+                .about("Add a file")
+                .arg(
+                    Arg::with_name("file")
+                    .help("File to add")
+                    .index(1)
+                    .multiple(true)
+                    .required(true),
+                ),
+    ).get_matches();
+
+    match m.subcommand() {
+        ("init", Some(..)) =>
+            match init::init() {
+                Ok(()) => println!("Repo Initialized."),
+                Err(..) => println!("Already Initialized!"),
+            },
+        ("add", Some(submatch)) => {
+            match add::add_all(&submatch.values_of("file").unwrap().collect()) {
+                Ok(()) => (),
+                Err(e) => println!("Error: {}", e),
+            }
+        }
+        _ => println!("Command not recognized.")
+    }
 }
