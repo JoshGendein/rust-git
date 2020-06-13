611c2cd0bc8c7ef8e6dd94be0fd4e3ae9919865f
Josh Gendein <joshua@gendein.com>
2020-06-13 15:52:53 -0700

    write_object now handles multiple object types

--- src/file.rs
+++ src/file.rs
@@ -6,7 +6,7 @@ use std::io;
 use std::io::{Read, Write};
 use std::path::{PathBuf, Path};
 
-use super::types::Blob;
+use super::types::{ Object, Tree };
 use super::error::Error;
 
 pub struct FileService {
@@ -59,21 +59,53 @@ impl FileService {
         Ok(self.gitr_dir.join(ref_path))
     }
 
-    pub fn write_blob(&self, blob: &Blob) -> io::Result<()> {
-        self.write_object(&blob.hash, &blob.data)
+    pub fn write_object(&self, object: &Object) -> io::Result<()> {
+        // &self, hash: &str, data: &Vec<u8>
+        match object {
+            Object::Blob(blob) => {
+                let blob_dir = self.object_dir.join(&blob.hash[..2]);
+                if !blob_dir.exists() {
+                    fs::create_dir(&blob_dir)?;
+                }
+                let blob_filename = blob_dir.join(&blob.hash[2..]);
+                let mut blob_f = File::create(&blob_filename)?;
+                blob_f.write_all(&blob.data)?;
+
+                Ok(())
+            },
+            Object::Tree(tree) => {
+                let tree_dir = self.object_dir.join(&tree.hash[..2]);
+                if !tree_dir.exists() {
+                    fs::create_dir(&tree_dir)?;
+                }
+                let tree_filename = tree_dir.join(&tree.hash[2..]);
+                let mut tree_f = File::create(&tree_filename)?;
+
+                tree_f.write_all(&Tree::build_tree_file(&tree.subtree)?)?;
+
+                Ok(())
+            },
+            Object::Commit(Commit) => {
+
+                Ok(())
+            },
+        }
     }
 
-    pub fn write_object(&self, hash: &str, data: &Vec<u8>) -> io::Result<()> {
-        // Objects are stored in a directory named by the first two characters of their hash.
-        let blob_dir = self.object_dir.join(&hash[..2]);
-        if !blob_dir.exists() {
-            fs::create_dir(&blob_dir)?;
-        }
-        // Files are named by their hash starting after the first two characters.
-        let blob_filename = blob_dir.join(&hash[2..]);
-        let mut blob_f = File::create(&blob_filename)?;
-        blob_f.write_all(data)?;
+    pub fn read_object(&self, object: &Object) -> io::Result<()> {
+        match object {
+            Object::Blob(Blob) => {
+            
+                Ok(())
+            },
+            Object::Tree(Tree) => {
 
-        Ok(())
+                Ok(())
+            },
+            Object::Commit(Commit) => {
+
+                Ok(())
+            },
+        }
     }
 }
