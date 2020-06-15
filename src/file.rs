de1dd1f8c7cab44cb87167e7b874597d6a4738f2
Josh Gendein <joshua@gendein.com>
2020-06-14 20:16:14 -0700

    added absoulte path

--- src/file.rs
+++ src/file.rs
@@ -5,14 +5,13 @@ use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
 use std::path::{PathBuf, Path};
-
-use super::types::{ Object, Tree };
 use super::error::Error;
 
 pub struct FileService {
     pub root_dir: PathBuf,
     pub gitr_dir: PathBuf,
     pub object_dir: PathBuf,
+    pub abs_dir: PathBuf,
 }
 
 impl FileService {
@@ -20,12 +19,14 @@ impl FileService {
         let root_dir = FileService::find_root()?;
         let gitr_dir = root_dir.join(".gitr").to_path_buf();
         let object_dir = gitr_dir.join("objects").to_path_buf();
+        let abs_dir = env::current_dir()?;
     
         Ok(
             FileService {
                 root_dir,
                 gitr_dir,
                 object_dir,
+                abs_dir,
             }
         )
     }
@@ -59,53 +60,63 @@ impl FileService {
         Ok(self.gitr_dir.join(ref_path))
     }
 
-    pub fn write_object(&self, object: &Object) -> io::Result<()> {
-        // &self, hash: &str, data: &Vec<u8>
-        match object {
-            Object::Blob(blob) => {
-                let blob_dir = self.object_dir.join(&blob.hash[..2]);
-                if !blob_dir.exists() {
-                    fs::create_dir(&blob_dir)?;
-                }
-                let blob_filename = blob_dir.join(&blob.hash[2..]);
-                let mut blob_f = File::create(&blob_filename)?;
-                blob_f.write_all(&blob.data)?;
-
-                Ok(())
-            },
-            Object::Tree(tree) => {
-                let tree_dir = self.object_dir.join(&tree.hash[..2]);
-                if !tree_dir.exists() {
-                    fs::create_dir(&tree_dir)?;
-                }
-                let tree_filename = tree_dir.join(&tree.hash[2..]);
-                let mut tree_f = File::create(&tree_filename)?;
-
-                tree_f.write_all(&Tree::build_tree_file(&tree.subtree)?)?;
-
-                Ok(())
-            },
-            Object::Commit(Commit) => {
-
-                Ok(())
-            },
+    pub fn write_object(&self, hash: &str, data: &Vec<u8>) -> Result<(), Error> {
+        // Check if first two character of hash dir exists.
+        let dir = self.object_dir.join(&hash[..2]);
+        if !dir.exists() {
+            fs::create_dir(&dir)?;
         }
+        // Write data to file with name last 38 characters of hash.
+        let filename = dir.join(&hash[2..]);
+        let mut object_file = File::create(&filename)?;
+        object_file.write_all(&data)?;
+        Ok(())
     }
 
-    pub fn read_object(&self, object: &Object) -> io::Result<()> {
-        match object {
-            Object::Blob(Blob) => {
-            
-                Ok(())
-            },
-            Object::Tree(Tree) => {
+    pub fn get_all_files_in_dir(&self, dir: &PathBuf) -> io::Result<Vec<PathBuf>> {
 
-                Ok(())
-            },
-            Object::Commit(Commit) => {
+        let mut files: Vec<PathBuf> = vec![];
 
-                Ok(())
-            },
+        self.visit_dirs(dir, &mut files)?;
+
+        return Ok(files);
+    }
+
+    fn visit_dirs(&self, dir: &Path, files: &mut Vec<PathBuf>) -> io::Result<()> {
+        if dir.is_dir() {
+            for entry in fs::read_dir(dir)? {
+                let entry = entry?;
+                let path = entry.path();
+                if self.is_ignored(&path) {
+                    continue;
+                }
+                if path.is_dir() {
+                    self.visit_dirs(&path, files)?;
+                } else {
+                    files.push(path);
+                }
+            }
         }
+        Ok(())
+    }
+
+    pub fn is_ignored (&self, entry: &PathBuf) -> bool {
+        // TODO: Check .gitrignore file.
+        // This is a terrible method but it works for now.
+        let entry = entry.to_path_buf();
+        let mut cmp_dir = PathBuf::from("");
+        let abs_path = self.abs_dir.to_path_buf();
+        cmp_dir.push(abs_path);
+        cmp_dir.push(".");
+        cmp_dir.push("target");
+        if entry == cmp_dir { return true };
+        cmp_dir.pop();
+        cmp_dir.push(".gitr");
+        if entry == cmp_dir { return true };
+        cmp_dir.pop();
+        cmp_dir.push(".git");
+        if entry == cmp_dir { return true };
+
+        return false;
     }
 }
